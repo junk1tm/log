3f0034ee00c9ebe72091c61dab92043cf4474c32
junk1tm <junk1tm@pm.me>
2021-11-12 17:30:45 +0300

    add logrus wrapper

---
+++ logrusimpl/logrus.go
@@ -0,0 +1,40 @@
+// Package logrusimpl contains logrus implementation of Logger interface.
+package logrusimpl
+
+import (
+	"github.com/junk1tm/log"
+	"github.com/sirupsen/logrus"
+)
+
+// NewLogger creates a new log.Logger from the provided logrus.Logger.
+func NewLogger(logger *logrus.Logger) log.Logger {
+	return &wrapper{
+		logger: logger,
+	}
+}
+
+type wrapper struct {
+	logger *logrus.Logger
+}
+
+func (w *wrapper) Debug(msg string, fields ...log.Field) {
+	w.logger.WithFields(logrusFields(fields)).Debug(msg)
+}
+
+func (w *wrapper) Info(msg string, fields ...log.Field) {
+	w.logger.WithFields(logrusFields(fields)).Info(msg)
+}
+
+func (w *wrapper) Error(msg string, fields ...log.Field) {
+	w.logger.WithFields(logrusFields(fields)).Error(msg)
+}
+
+func logrusFields(fields []log.Field) map[string]interface{} {
+	lf := make(map[string]interface{}, len(fields))
+
+	for _, field := range log.FlattenFields(fields) {
+		lf[field.Key] = field.Value
+	}
+
+	return lf
+}
