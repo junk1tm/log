8ab8a4fa4840447bed115227f1589fdaf8b50796
Tom <73077675+junk1tm@users.noreply.github.com>
2021-11-15 18:05:48 +0300

    add stdlog implementation (#8)

---
+++ stdlogimpl/example_test.go
@@ -0,0 +1,25 @@
+package stdlogimpl_test
+
+import (
+	stdlog "log"
+	"os"
+
+	"github.com/junk1tm/log"
+
+	"github.com/junk1tm/log/stdlogimpl"
+)
+
+func ExampleNewLogger() {
+	// configure stdlog logger here:
+	sl := stdlog.New(os.Stdout, "", 0)
+
+	logger := stdlogimpl.NewLogger(sl)
+	logger.Debug("example 1", log.Int("foo", 1))
+	logger.Info("example 2", log.Int("bar", 2))
+	logger.Error("example 3", log.Int("baz", 3))
+
+	// output:
+	// [DEBUG] example 1 foo=1
+	// [INFO] example 2 bar=2
+	// [ERROR] example 3 baz=3
+}
---
+++ stdlogimpl/go.mod
@@ -0,0 +1,5 @@
+module github.com/junk1tm/log/stdlogimpl
+
+go 1.17
+
+require github.com/junk1tm/log v0.4.0
